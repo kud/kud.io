305f7fa689e69ed693fa47917b6d05542a1a64d3
Erwann Mest <m@kud.io>
2026-09-04 12:57:45 +0100

    ♻️ refactor(blog): use next-view-transitions Link
    - Swap next/link for next-view-transitions' Link in app/blog/layout.tsx and components/blog-index.tsx
    - Enables animated view transitions on blog navigation

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react"
-import Link from "next/link"
+import { Link } from "next-view-transitions"
 import {
   BLOG_ROOT_ID,
   BLOG_THEME_SCRIPT,
--- components/blog-index.tsx
+++ components/blog-index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Link from "next/link"
+import { Link } from "next-view-transitions"
 import { useMemo, useState } from "react"
 
 export type IndexEntry = {
